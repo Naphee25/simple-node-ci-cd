1bd852e2afc6a5eb837dc0552fd46e0c99da1554
Nafisah Abidemi Abdulkadir <65966171+Naphee25@users.noreply.github.com>
2025-05-07 13:21:35 +0200

    Initial Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -6,18 +6,18 @@ import css from "@eslint/css";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  // Globally ignore dist and node_modules
   {
-    ignores: ["dist/**", "node_modules/**"],
+    ignores: ["package.json", "package-lock.json", "node_modules/**"],
   },
-
-  // JS and JSX files with JS plugin
   {
     files: ["**/*.{js,mjs,cjs,jsx}"],
     plugins: { js },
     extends: ["js/recommended"],
     languageOptions: {
-      globals: { ...globals.browser, ...globals.node },
+      globals: {
+        ...globals.browser,
+        ...globals.node,
+      },
       parserOptions: {
         ecmaVersion: "latest",
         sourceType: "module",
@@ -28,54 +28,42 @@ export default defineConfig([
     }
   },
 
-
-  // JSON files
+  {
+    files: ["**/*.test.js", "**/*.spec.js"],
+    languageOptions: {
+      globals: {
+        ...globals.jest
+      }
+    }
+  },
   {
     files: ["**/*.json"],
     plugins: { json },
     language: "json/json",
     extends: ["json/recommended"]
   },
-
-  // JSONC files
   {
     files: ["**/*.jsonc"],
     plugins: { json },
     language: "json/jsonc",
     extends: ["json/recommended"]
   },
-
-  // JSON5 files
   {
     files: ["**/*.json5"],
     plugins: { json },
     language: "json/json5",
     extends: ["json/recommended"]
   },
-
-  // Markdown files
   {
     files: ["**/*.md"],
     plugins: { markdown },
     language: "markdown/gfm",
     extends: ["markdown/recommended"]
   },
-
-  // CSS files
   {
     files: ["**/*.css"],
     plugins: { css },
     language: "css/css",
     extends: ["css/recommended"]
-  },
-
-  // Jest environment for test files
-  {
-    files: ["**/*.test.js", "**/*.spec.js"], // Add pattern for test files
-    languageOptions: {
-      env: {
-        jest: true // Enable Jest globals
-      }
-    }
   }
 ]);

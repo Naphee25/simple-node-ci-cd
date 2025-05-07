79491c4a146abc74b03e4afe39df16d2541f29f0
Nafisah Abidemi Abdulkadir <65966171+Naphee25@users.noreply.github.com>
2025-05-07 13:06:47 +0200

    Initial Commit

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,15 +3,69 @@ import globals from "globals";
 import json from "@eslint/json";
 import markdown from "@eslint/markdown";
 import css from "@eslint/css";
+import pluginReact from "eslint-plugin-react";
 import { defineConfig } from "eslint/config";
 
+export default defineConfig({
+  ignores: ["dist/**", "node_modules/**"],
 
-export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs,jsx}"], plugins: { js }, extends: ["js/recommended"] },
-  { files: ["**/*.{js,mjs,cjs,jsx}"], languageOptions: { globals: {...globals.browser, ...globals.node} } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
-  { files: ["**/*.jsonc"], plugins: { json }, language: "json/jsonc", extends: ["json/recommended"] },
-  { files: ["**/*.json5"], plugins: { json }, language: "json/json5", extends: ["json/recommended"] },
-  { files: ["**/*.md"], plugins: { markdown }, language: "markdown/gfm", extends: ["markdown/recommended"] },
-  { files: ["**/*.css"], plugins: { css }, language: "css/css", extends: ["css/recommended"] },
-]);
+  overrides: [
+    {
+      files: ["**/*.{js,mjs,cjs,jsx}"],
+      plugins: { js },
+      extends: ["js/recommended"],
+      languageOptions: {
+        globals: { ...globals.browser, ...globals.node },
+        parserOptions: {
+          ecmaVersion: "latest",
+          sourceType: "module",
+          ecmaFeatures: {
+            jsx: true
+          }
+        }
+      }
+    },
+    {
+      files: ["**/*.{js,jsx}"],
+      plugins: { react: pluginReact },
+      rules: {
+        ...pluginReact.configs.recommended.rules
+      },
+      settings: {
+        react: {
+          version: "detect"
+        }
+      }
+    },
+    {
+      files: ["**/*.json"],
+      plugins: { json },
+      language: "json/json",
+      extends: ["json/recommended"]
+    },
+    {
+      files: ["**/*.jsonc"],
+      plugins: { json },
+      language: "json/jsonc",
+      extends: ["json/recommended"]
+    },
+    {
+      files: ["**/*.json5"],
+      plugins: { json },
+      language: "json/json5",
+      extends: ["json/recommended"]
+    },
+    {
+      files: ["**/*.md"],
+      plugins: { markdown },
+      language: "markdown/gfm",
+      extends: ["markdown/recommended"]
+    },
+    {
+      files: ["**/*.css"],
+      plugins: { css },
+      language: "css/css",
+      extends: ["css/recommended"]
+    }
+  ]
+});
